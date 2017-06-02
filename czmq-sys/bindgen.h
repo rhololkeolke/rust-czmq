d5b4d79bf602dbf19d3020b35aad1ab3becb664b
Devin Schwab <digidevin@gmail.com>
2017-06-02 00:52:47 -0400

    Added an optional env var that specifies CZMQ include search path

--- czmq-sys/bindgen.h
+++ czmq-sys/bindgen.h
@@ -1,3 +1,1 @@
-// Include C source files where we need to resolve their struct
-// members.
 #include <czmq.h>
--- czmq-sys/build.rs
+++ czmq-sys/build.rs
@@ -3,6 +3,7 @@ extern crate pkg_config;
 fn main() {
     // find version 4 of libczmq and output the proper linker flags
     pkg_config::Config::new().atleast_version("4.0").probe("libczmq").unwrap();
+    
 
     // either copy over the bundled bindgen bindings or run bindgen now
     bindings::write_to_out_dir();
@@ -36,6 +37,7 @@ mod bindings {
     pub fn write_to_out_dir() {
         let mut builder = bindgen::Builder::default()
             .no_unstable_rust()
+            .clang_arg(format!("-I{}", env::var("CZMQ_INCLUDE_DIR").unwrap_or(String::from(""))))
             .header("bindgen.h")
             .opaque_type("zmq_msg_t")
             .hide_type("IPPORT_RESERVED")
